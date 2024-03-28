a9f89c6554f38905264bfd0e2b1d4ffaf2e506cc
shui9527 <89987271+shui9527@users.noreply.github.com>
2024-03-28 17:54:51 +0800

    Update pre_upload.py

--- pre_upload.py
+++ pre_upload.py
@@ -1,6 +1,5 @@
 import os
 import json
-import re
 import asyncio
 from douyin_upload import upload_video
 
@@ -13,26 +12,19 @@ async def pre_upload():
             with open(folder_path + '/' + file_name.split('.')[0]+'.json', 'r', encoding='utf-8') as info:
                 info_content = info.read()
                 info_dict = json.loads(info_content)
-                video_path = info_dict.get('video_path')
-                video_path = video_path + '/' + file_name
+                video_path = info_dict.get('video_save_path')
+                print(video_path)
+                video_path = video_path + '.' + file_name.split('.')[1]
+
                 video_name = info_dict.get('title')
-                description = await remove_lines_with_http_https(info_dict.get('description', ''))
-                tags = info_dict.get('tags', '')
+                description = info_dict.get('description')
+                tags = info_dict.get('tags')
 
                 # 调用抖音上传函数
                 if video_path:
-                    await upload_video(video_path=video_path, video_name=video_name, description=description, tags=tags, storage_state=None, headless=False)
+                    await upload_video(video_path=video_path, video_name=video_name, description=description, tags=tags,
+                                       storage_state=None, headless=False)
 
-
-async def remove_lines_with_http_https(input_text):
-    lines = input_text.splitlines()  # 以行为单位拆分多行内容
-
-    # 使用正则表达式匹配开头为 'http' 或 'https' 的行进行过滤
-    filtered_lines = [line for line in lines if not re.match(r'^\s*(http|https)', line)]
-
-    output_text = '\n'.join(filtered_lines)  # 将剩余的行重新组合成一个字符串
-
-    return output_text
 
 if __name__ == '__main__':
     asyncio.run(pre_upload())
