a0258c7e91d07b77880cb578b79939a1833c561a
shui9527 <89987271+shui9527@users.noreply.github.com>
2024-03-30 19:11:20 +0800

    Update pre_upload.py

--- pre_upload.py
+++ pre_upload.py
@@ -9,13 +9,13 @@ async def pre_upload():
     # 通过视频文件名找到同名的记录视频信息的json文件获取视频信息
     for file_name in os.listdir(folder_path):
         if file_name.endswith('.mp4') or file_name.endswith('.avi') or file_name.endswith('.mkv'):
-            with open(folder_path + '/' + file_name.split('.')[0]+'.json', 'r', encoding='utf-8') as info:
+            json_name = os.path.splitext(file_name)[0]
+            print(json_name)
+            with open(folder_path + '/' + os.path.splitext(file_name)[0]+'.json', 'r', encoding='utf-8') as info:
                 info_content = info.read()
                 info_dict = json.loads(info_content)
                 video_path = info_dict.get('video_save_path')
-                print(video_path)
-                video_path = video_path + '.' + file_name.split('.')[1]
-
+                video_path = video_path + os.path.splitext(file_name)[1]
                 video_name = info_dict.get('title')
                 description = info_dict.get('description')
                 tags = info_dict.get('tags')
